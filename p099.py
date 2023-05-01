8a38d88b821b5ca0c62ef5a55f7c7cdf60d3e315
salastro <salah2112004@gmail.com>
2023-05-01 04:18:46 +0300

    p099: use tuples more efficiently

--- p099.py
+++ p099.py
@@ -2,12 +2,12 @@ from math import log
 
 def largest_exponential(file: str) -> int:
     """Return the line number of the pair with the largest exponential."""
-    # parse the file into a tuple
+    # parse the file
     with open(file, 'r') as f:
-        pairs = tuple([tuple(map(int, line.split(','))) for line in f])
+        pairs = *(tuple(map(int, line.split(','))) for line in f),
 
     # multiply the exponent by the log of the base
-    logs = [pair[1] * log(pair[0]) for pair in pairs] # list of logs
+    logs = *(pair[1] * log(pair[0]) for pair in pairs), # list of logs
     return logs.index(max(logs)) + 1 # return the line number
 
 print(largest_exponential('data/p099_base_exp.txt'))
